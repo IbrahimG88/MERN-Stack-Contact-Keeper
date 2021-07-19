ff3d1b2a3cde03919ecc0e6627f1746de2932fcb
IbrahimG88 <goharyibrahim@gmail.com>
2021-07-19 17:37:28 +0200

    Hash Passwords, register route, add a user in the mongodb users collection

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,8 @@ const User = require("../models/User");
 
 const { check, validationResult } = require("express-validator");
 
+const bcrypt = require("bcryptjs");
+
 /* POST api/users, means it will be using this route since in server.js we have:
 app.use("/api/users", require("./routes/users"));
 */
@@ -24,14 +26,45 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     //  res.send(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("passed");
+    const { name, email, password } = req.body;
+
+    try {
+      // we are checking if the user already exists before
+      // seacrh for a User that has an email that matches the one from the request body
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+      // if the user doesn't exist: create a new user using the User mongoose model
+      // same name: name, email: email
+      // we set the new User name as from this const:     const { name, email, password } = req.body;
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // now we want to encrypt or hash the password before passing it to the database, using bcrypt
+      const salt = await bcrypt.genSalt(10);
+      // genSalt generates a salt
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("User saved");
+      // you can check its saved in the mongodb atlas cluster and you will find the password hashed
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
